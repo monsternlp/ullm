ec9a0c1638200efa5bfebe46d7e314b8142207fc
Linusp <linusp1024@gmail.com>
2024-05-28 13:18:27 +0800

    Updated `BaichuanModel` to support tools calling

--- ullm/baichuan.py
+++ ullm/baichuan.py
@@ -9,32 +9,38 @@ from .base import (
     RemoteLanguageModel,
     RemoteLanguageModelMetaInfo,
     TextPart,
+    Tool,
+    ToolChoice,
     ToolMessage,
     UserMessage,
 )
 from .openai import (
     OpenAICompatibleModel,
+    OpenAIFunctionObject,
     OpenAIRequestBody,
+    OpenAIToolCall,
 )
 
 
 class BaichuanChatMessage(BaseModel):
-    role: Literal["user", "assistant"]
+    role: Literal["system", "user", "assistant", "tool"]
     content: str
+    # NOTE: https://platform.baichuan-ai.com/docs/api#12 文档上说 tool_calls 是 string 类型
+    #       但将 tool_calls json.dumps 后调用 API 会报错，按 OpenAI 的形式传入则正常
+    tool_calls: Optional[List[OpenAIToolCall]] = None
+    tool_call_id: Optional[str] = None
 
     @classmethod
     def from_standard(cls, message: ChatMessage):
         if isinstance(message, AssistantMessage):
             content = message.content
-            if message.tool_calls:  # NOTE: API 文档中未定义相关行为
-                content = ""
+            tool_calls = None
+            if message.tool_calls:
+                content, tool_calls = "", []
                 for tool_call in message.tool_calls:
-                    content += (
-                        f"You should call the function `{tool_call.function.name}` with arguments: "
-                        f"{tool_call.function.arguments}\n"
-                    )
+                    tool_calls.append(OpenAIToolCall.from_standard(tool_call))
 
-            return BaichuanChatMessage(role="assistant", content=content.strip())
+            return cls(role="assistant", content=content.strip(), tool_calls=tool_calls)
 
         if isinstance(message, UserMessage):
             content = ""
@@ -44,14 +50,10 @@ class BaichuanChatMessage(BaseModel):
                 else:
                     pass
 
-            return BaichuanChatMessage(role="user", content=content.strip())
+            return cls(role="user", content=content.strip())
 
         if isinstance(message, ToolMessage):
-            content = (
-                f"I called the function `{message.tool_name}` and "
-                f"the response of that function is: {message.tool_result}"
-            )
-            return cls(role="user", content=content)
+            return cls(role="tool", content=message.tool_result, tool_call_id=message.tool_call_id)
 
 
 class BaichuanRetrievalObject(BaseModel):
@@ -59,9 +61,23 @@ class BaichuanRetrievalObject(BaseModel):
     answer_mode: Optional[Literal["knowledge-base-only"]] = None
 
 
+class BaichuanWebSearchObject(BaseModel):
+    enable: Optional[bool] = False
+    search_mode: Optional[Literal["performance_first", "quality_first"]] = "performance_first"
+
+
+BaichuanFunctionObject = OpenAIFunctionObject
+
+
 class BaichuanTool(BaseModel):
-    type: Literal["retrieval"] = "retrieval"
-    retrieval: BaichuanRetrievalObject
+    type: Optional[Literal["web_search", "retrieval", "function"]] = None
+    retrieval: Optional[BaichuanRetrievalObject] = None
+    web_search: Optional[BaichuanRetrievalObject] = None
+    function: Optional[BaichuanFunctionObject] = None
+
+    @classmethod
+    def from_standard(cls, tool: Tool):
+        return cls(type=tool.type, function=BaichuanFunctionObject.from_standard(tool.function))
 
 
 class BaichuanRequestBody(OpenAIRequestBody):
@@ -76,16 +92,15 @@ class BaichuanRequestBody(OpenAIRequestBody):
     response_format: Optional[Any] = Field(None, exclude=True)
     seed: Optional[Any] = Field(None, exclude=True)
     stop: Optional[Any] = Field(None, exclude=True)
-    tool_choice: Optional[Any] = Field(None, exclude=True)
     user: Optional[Any] = Field(None, exclude=True)
 
     ## different parameters
     messages: conlist(BaichuanChatMessage, min_length=1)
     tools: Optional[List[BaichuanTool]] = None
+    tool_choice: Optional[Literal["auto", "none"]] = None
 
     ## Baichuan-specific parameters
     top_k: Optional[conint(ge=0, le=20)] = None
-    with_search_enhance: Optional[bool] = False
 
 
 @RemoteLanguageModel.register("baichuan")
@@ -135,20 +150,37 @@ class BaichuanModel(OpenAICompatibleModel):
     ) -> Dict[str, Any]:
         messages = [self._convert_message(message) for message in messages]
         if system:
-            # NOTE: 百川不支持 system 参数
-            messages = [BaichuanChatMessage(role="user", content=system)] + messages
+            messages = [BaichuanChatMessage(role="system", content=system)] + messages
 
         return {"messages": messages}
+
+    @validate_call
+    def _convert_tools(
+        self, tools: Optional[List[Tool]] = None, tool_choice: Optional[ToolChoice] = None
+    ) -> Dict[str, Any]:
+        if tools:
+            tools = [BaichuanTool.from_standard(tool) for tool in tools]
+
+        baichuan_tool_choice = None
+        if tools and tool_choice is not None:
+            baichuan_tool_choice = tool_choice.mode
+            if baichuan_tool_choice == "any":
+                baichuan_tool_choice = "auto"
+
+        return {"tools": tools, "tool_choice": baichuan_tool_choice}
 
     def _convert_generation_config(
         self, config: GenerateConfig, system: Optional[str] = None
     ) -> Dict[str, Any]:
-        return {
+        generation_config = {
             "model": self.model.replace("-online", ""),
             "max_tokens": config.max_output_tokens or self.config.max_output_tokens,
             "stop": config.stop_sequences or self.config.stop_sequences,
             "temperature": config.temperature or self.config.temperature,
             "top_p": config.top_p or self.config.top_p,
             "top_k": config.top_k or self.config.top_k,
-            "with_search_enhance": self.is_online_model(),
         }
+        if self.is_online_model():
+            generation_config["tools"] = [{"type": "web_search", "web_search": {"enable": True}}]
+
+        return generation_config
