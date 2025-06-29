35452b79291a6bfce591735083eb7d9f8cf7cb03
Linusp <linusp1024@gmail.com>
2025-06-29 10:16:31 +0800

    support thinking option in `ByteDanceModel`

--- ullm/bytedance.py
+++ ullm/bytedance.py
@@ -1,6 +1,6 @@
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Literal, Optional
 
-from pydantic import Field
+from pydantic import BaseModel, Field
 
 from .base import (
     GenerateConfig,
@@ -13,8 +13,15 @@ from .openai import (
 )
 
 
+class ByteDanceThinkingOption(BaseModel):
+    type: Optional[Literal["disabled", "enabled", "auto"]] = "auto"
+
+
 class ByteDanceRequestBody(OpenAIRequestBody):
     # reference: https://www.volcengine.com/docs/82379/1298454
+    ## ByteDance-specific parameters
+    thinking: Optional[ByteDanceThinkingOption] = Field(None)
+
     ## excluded parameters
     n: Optional[Any] = Field(None, exclude=True)
     response_format: Optional[Any] = Field(None, exclude=True)
@@ -105,7 +112,7 @@ class ByteDanceModel(OpenAICompatibleModel):
     def _convert_generation_config(
         self, config: GenerateConfig, system: Optional[str] = None
     ) -> Dict[str, Any]:
-        return {
+        generation_config = {
             "model": self.model,
             "frequency_penalty": config.frequency_penalty,
             "max_tokens": config.max_output_tokens or self.config.max_output_tokens,
@@ -115,3 +122,12 @@ class ByteDanceModel(OpenAICompatibleModel):
             "temperature": config.temperature or self.config.temperature,
             "top_p": config.top_p or self.config.top_p,
         }
+        if config.thinking_type:
+            if self.model in ("doubao-seed-1-6-250615", "doubao-1-5-thinking-vision-pro-250428"):
+                generation_config["thinking"] = {"type": config.thinking_type}
+            elif self.model in ("doubao-seed-1-6-flash-250615", "doubao-1-5-thinking-pro-m-250428"):
+                generation_config["thinking"] = {
+                    "type": config.thinking_type if config.thinking_type != "auto" else "disabled"
+                }
+
+        return generation_config
